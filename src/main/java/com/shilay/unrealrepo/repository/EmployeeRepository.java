481eb960ea6f2ea20faa67dd4725e248834c5f60
Viktor Shilay <viktor.shilay@gmail.com>
2023-10-24 21:48:10 +0300

    reformat repo

--- src/main/java/com/shilay/unrealrepo/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shilay.unrealrepo.repository;
-
-import com.shilay.unrealrepo.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
